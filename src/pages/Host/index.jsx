cd93d4becc7eed8c2281bb5b433b39d732851ecb
Allisson100 <allisson.boiane@outlook.com>
2025-03-26 21:38:16 -0300

    ngrok

--- src/pages/Host/index.jsx
+++ src/pages/Host/index.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
-const socket = io("https://5274-177-72-141-5.ngrok-free.app", {
+const socket = io("https://b6bc-177-72-141-5.ngrok-free.app", {
   transports: ["websocket", "polling"], // Garante compatibilidade
   reconnectionAttempts: 5, // Tenta reconectar até 5 vezes
   reconnectionDelay: 1000, // Espera 1 segundo entre tentativas
